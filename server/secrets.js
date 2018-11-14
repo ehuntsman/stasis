4dd6e3d95ee263fa35c8b206fcb123e0eaac0c99
Elizabeth <ehuntsman@wavetronix.com>
2018-11-14 14:44:38 -0700

    removed secrets in github

---
+++ server/secrets.js
@@ -0,0 +1,11 @@
+module.exports = {
+  connectionString: "postgres://lwnvovtahfhykf:9b9ac52ad83eb03f3a5435dfeefa40b91675ddefec5c02221686dff74203db87@ec2-50-16-241-91.compute-1.amazonaws.com:5432/d9kt8gqq22q9vr?ssl=true",
+  domain: "ehuntsman.auth0.com",
+  clientID: "k0ryXjjHxkwSaeVqaqsr2Ga3d4caYwHT",
+  clientSecret: "yKLE7eGKAdiMQD2I1UrV1rYdg09QyySAVEjmuF35PdOwYGtBRPwEWM2b5e6JVT2-",
+  sessionSecret: 'theCake15aL13',
+  auth0Callback: 'http://localhost:3000/auth/callback',
+  authRedirect: 'http://localhost:3001/#myteam',
+  logoutredirect: 'http://localhost:3001/',
+  port: 3001
+}
